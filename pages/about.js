86a5b87a363ebe42a0efcdad7fee958171855dc9
Rob Peckham <peckham.rob@gmail.com>
2020-11-14 21:03:37 -0500

    changed from functional to class component

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,24 @@
+import React from 'react';
 
+// const About = () => {
+//     const message = 'hello world';
+//     return (
+//         <h1>Hello About Page - {message}</h1>
+//     )
+// }
 
-const About = () => (
-    <h1>Hello About Page</h1>
-)
+// const About = () => {
+//   const message = 'Hello World'
+//   return React.createElement('div', null, 'I am generating this with cE')
+// }
+
+class About extends React.Component {
+
+  render() {
+    return (
+      <h1>Hello I am a Class component</h1>
+    )
+  }
+}
 
 export default About
